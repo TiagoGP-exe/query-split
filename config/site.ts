87adf0639cbf5e43b9ea37acf8326c73e0b494fe
Tiago Guimarães Pinto <tguimaraespinto@gmail.com>
2025-04-21 15:35:13 -0400

    refactor: ajustment og

--- config/site.ts
+++ config/site.ts
@@ -4,8 +4,8 @@ export const siteConfig: SiteConfig = {
   name: "Query Split",
   description:
     "Query Split é um site de artigos para os alunos do IFMT Campus Cuiabá - Octayde Jorge da Silva.",
-  url: "https://ifmt-hub.vercel.app/",
-  ogImage: "https://ifmt-hub.vercel.app/og.jpg",
+  url: "https://query-split.vercel.app/",
+  ogImage: "https://query-split.vercel.app/og.jpg",
   links: {
     twitter: "https://twitter.com/tiagogp_exe",
     github: "https://github.com/tiagogp-exe/ifmthub",
